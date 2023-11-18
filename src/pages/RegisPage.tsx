654a75766ad939bfd5f9903ef082b1d692c70539
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-11-18 23:53:07 +0700

    HAR-8 Create Regis Page.

---
+++ src/pages/RegisPage.tsx
@@ -0,0 +1,15 @@
+import LoginRegis from '../components/LoginRegis'
+
+const RegisPage = () => {
+    return (
+        <>
+            <LoginRegis
+                title='Daftar'
+                isLogin={false}
+                tagLine='Buat undangan nikah digital sendiri hanya 5 menit.'
+            />
+        </>
+    )
+}
+
+export default RegisPage
