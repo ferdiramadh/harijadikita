ca42fc05747b3ff77166f09ce81608b0da98502a
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-11-18 22:10:50 +0700

    HAR-8 Create login page and its styles.

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import Logo from '../components/Logo'
+
+export default function LoginPage() {
+  return (
+    <section className='login'>
+        <Logo />
+        <div className='upperSection'>
+            <h1>Masuk</h1>
+        </div>
+    </section>
+  )
+}
