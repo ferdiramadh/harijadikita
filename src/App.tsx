cf9c5a1467a0c16487ebd47b45eb2470d1fd1363
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-11-18 14:56:10 +0700

    HAR-8 Try to implement sass/scss into the project.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 function App() {
   return (
     <React.Fragment>
+      <main>
+        <h1 className='test'>Tests</h1>
+      </main>
+
     </React.Fragment>
   );
 }
