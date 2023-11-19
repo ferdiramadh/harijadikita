8a079630282f75f7fbf224711814ed5295900449
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-11-19 13:19:01 +0700

    HAR-9 Refactoring pages.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,12 @@
 import LoginRegis from '../components/LoginRegis'
 
 const LoginPage = () => {
-    return (
-        <>
-            <LoginRegis
-                title='Masuk'
-                isLogin={true}
-                tagLine='Lanjutkan buat undangan nikah digital kamu hanya 5 menit.'
-                isEmailVerification={false}
-            />
-        </>
-    )
+    return <LoginRegis
+        title='Masuk'
+        isLogin={true}
+        tagLine='Lanjutkan buat undangan nikah digital kamu hanya 5 menit.'
+        isEmailVerification={false}
+    />
 }
 
 export default LoginPage
--- src/pages/RegisPage.tsx
+++ src/pages/RegisPage.tsx
@@ -1,16 +1,12 @@
 import LoginRegis from '../components/LoginRegis'
 
 const RegisPage = () => {
-    return (
-        <>
-            <LoginRegis
-                title='Daftar'
-                isLogin={false}
-                tagLine='Buat undangan nikah digital sendiri hanya 5 menit.'
-                isEmailVerification={false}
-            />
-        </>
-    )
+    return <LoginRegis
+        title='Daftar'
+        isLogin={false}
+        tagLine='Buat undangan nikah digital sendiri hanya 5 menit.'
+        isEmailVerification={false}
+    />
 }
 
 export default RegisPage
