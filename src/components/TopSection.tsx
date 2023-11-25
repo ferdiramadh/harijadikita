4495c202d7238aeb65c91707888d93f0110955ed
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-11-25 12:52:24 +0700

    HAR-9 Add prop for lokasi pernikahan suggestion.

--- src/components/TopSection.tsx
+++ src/components/TopSection.tsx
@@ -3,15 +3,17 @@ import Logo from "./Logo"
 type TopSectionProps = {
     title: string
     tagline: string
+    locAd?: string
 }
 
-const TopSection = ({ title, tagline }: TopSectionProps) => {
+const TopSection = ({ title, tagline, locAd }: TopSectionProps) => {
     return (
         <div className="top-sec-wrapper">
             <Logo />
             <h1 className="top-title">{title}</h1>
             <div className='tagline'>
                 <p>{tagline}</p>
+                {locAd && <p className="loc-ad">{locAd} <a href="."><span>lihat caranya</span></a></p>}
             </div>
         </div>
     )
