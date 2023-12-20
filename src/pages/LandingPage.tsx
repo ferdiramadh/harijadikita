fd5647092f6c04de30f0b5ef93e0cb40e72d65b1
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-12-20 22:06:31 +0700

    HAR-18 Create temporer landing page.

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <h1>Landing Page Ceritanya</h1>
+        <Link to='/signin'>Sign In</Link>
+    </div>
+
+  )
+}
+
+export default LandingPage
