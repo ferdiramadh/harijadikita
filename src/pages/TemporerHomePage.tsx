47bdfc72bbf334cdf04eef9a30555ccab8f2005b
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-12-22 23:25:41 +0700

    HAR-18 Update displayed name.

--- src/pages/TemporerHomePage.tsx
+++ src/pages/TemporerHomePage.tsx
@@ -1,7 +1,7 @@
 import { UserAuth } from '../context/AuthContext'
 
 const TemporerHomePage = () => {
-  const { user, logOut } = UserAuth()
+  const { user, logOut, userName } = UserAuth()
   const handleLogout = async () => {
     try {
       await logOut()
@@ -13,8 +13,8 @@ const TemporerHomePage = () => {
   return (
     <div style={{ display: 'flex', height: '100%', justifyContent: 'flex-start', alignItems: 'flex-start', padding: 20 }}>
       <div>
-        <h3>Welcome, {user?.displayName}</h3>
-        {user?.displayName ? <button onClick={handleLogout}>Logout</button> : null}
+        <h3>Welcome, {user?.displayName || userName}</h3>
+        {user ? <button onClick={handleLogout}>Logout</button> : null}
       </div>
     </div>
   )
