a11f15079240dfee7a937c205409734f723dac73
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-12-22 19:50:54 +0700

    HAR-18 Create Private Routes.

---
+++ src/utils/PrivateRoutes.tsx
@@ -0,0 +1,11 @@
+import { Outlet, Navigate } from "react-router-dom"
+import { UserAuth } from "../context/AuthContext"
+
+const PrivateRoutes = () => {
+    const { user } = UserAuth()
+    return (
+        user ? <Outlet /> : <Navigate to='/' />
+    )
+}
+
+export default PrivateRoutes
