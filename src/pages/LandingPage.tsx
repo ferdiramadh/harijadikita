43538323478c377f2d9b2d81cec3d61aba77c302
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-12-22 19:49:41 +0700

    HAR-18 Update landing page.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <div>
+    <div style={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center', padding: 20 }}>
+      <div>
         <h1>Landing Page Ceritanya</h1>
         <Link to='/signin'>Sign In</Link>
+      </div>
     </div>
 
   )
