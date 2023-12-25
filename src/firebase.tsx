270681a9a8e4a0628739535824ff103d778297f0
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-12-25 19:16:26 +0700

    HAR-18 Initiate firestore.

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,11 +16,14 @@ const firebaseConfig = {
   messagingSenderId: "620848935395",
   appId: "1:620848935395:web:8677c4b4f75a0573f9f0d6",
   measurementId: "G-N26687T7HX"
-};
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app)
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app)
