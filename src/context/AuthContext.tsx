b60cebfdd265641f8d6c6c124971200b855e7928
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-12-25 19:17:42 +0700

    HAR-18 Implement add and read data users collection on firestore.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -10,6 +10,8 @@ import {
     FacebookAuthProvider
 } from 'firebase/auth'
 import { auth } from "../firebase"
+import { DocumentData, addDoc, collection, getDocs, query, where } from "firebase/firestore"
+import { db } from "../firebase"
 
 interface ChildrenProps {
     children?: ReactNode
@@ -21,13 +23,21 @@ type UserContextType = {
     userName: string
     setUserName: React.Dispatch<React.SetStateAction<string>>
     facebookSignIn: () => void
+    userAcc: DocumentData | UserCollectionProps | undefined
+    setUserAcc: React.Dispatch<React.SetStateAction<DocumentData | UserCollectionProps | undefined>>
 }
-
+type UserCollectionProps = {
+    email: string
+    displayName: string
+    uid: string
+    email_verified: boolean
+}
 const AuthContext = createContext<UserContextType>({} as UserContextType)
 
 export const AuthContextProvider = ({ children }: ChildrenProps) => {
     const [user, setUser] = useState<any>({})
     const [userName, setUserName] = useState('')
+    const [userAcc, setUserAcc] = useState<UserCollectionProps | DocumentData>()
     const googleSignIn = () => {
         const provider = new GoogleAuthProvider()
         signInWithPopup(auth, provider)
@@ -39,6 +49,48 @@ export const AuthContextProvider = ({ children }: ChildrenProps) => {
     const logOut = () => {
         signOut(auth)
     }
+
+    const getData = async () => {
+        try {
+            const q = query(collection(db, "users"), where("uid", "==", user.uid))
+
+            const querySnapshot = await getDocs(q)
+            const docLength = querySnapshot.docs.length
+            if (docLength == 1) {
+                console.log('cari cuy')
+                querySnapshot.forEach((doc) => {
+                    const data = doc.data()
+                    setUserAcc(data)
+
+                })
+            } else if (docLength == 0) {
+                console.log('nambah nih')
+                addUser(user.email, user.displayName, user.uid, user.emailVerified)
+                setUserAcc({
+                    displayName: user.displayName,
+                    email: user.email,
+                    email_verified: user.emailVerified,
+                    uid: user.uid
+                })
+            }
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    const addUser = async (email: string, name: string, uid: string, email_ver: boolean) => {
+        try {
+            const docRef = await addDoc(collection(db, "users"), {
+                email: email,
+                displayName: userName ? userName : name,
+                uid: uid,
+                email_verified: email_ver
+            })
+            console.log(docRef)
+        } catch (err) {
+            console.log(err)
+        }
+    }
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser)
@@ -49,8 +101,14 @@ export const AuthContextProvider = ({ children }: ChildrenProps) => {
             unsubscribe()
         }
     }, [])
+
+    useEffect(() => {
+        if (user != null) {
+            getData()
+        }
+    }, [user])
     return (
-        <AuthContext.Provider value={{ googleSignIn, logOut, user, userName, setUserName, facebookSignIn }}>
+        <AuthContext.Provider value={{ googleSignIn, logOut, user, userName, setUserName, facebookSignIn, userAcc, setUserAcc }}>
             {children}
         </AuthContext.Provider>
     )
