da48bf89272bdb41d80b402215a84eda29de09b1
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-12-25 17:58:23 +0700

    HAR-18 Check if user has signed in on landing page.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const LandingPage = () => {
+  const { user } = UserAuth()
+  const checkSignIn = () => {
+    if(user == null) return '/signin'
+    return '/home'
+  }
   return (
     <div style={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center', padding: 20 }}>
       <div>
         <h1>Landing Page Ceritanya</h1>
-        <Link to='/signin'>Sign In</Link>
+        <Link to={checkSignIn()}>Sign In</Link>
       </div>
     </div>
 
