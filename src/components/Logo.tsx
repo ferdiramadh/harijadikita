13bf0883a04e426941096e4617a0b27901e7b234
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-12-25 21:34:28 +0700

    HAR-18 Navigate logo to landing page.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,10 @@
 import logoImage from '../assets/logo/harijadikita_logo.png'
+import { useNavigate } from 'react-router-dom'
 
 export default function Logo() {
+  const navigate = useNavigate()
   return (
-    <div className='logo_wrapper'>
+    <div className='logo_wrapper' onClick={() => navigate('/')}>
         <img className='logo_image' src={logoImage} alt='logo'/>
         <h1 className='logo_text'>harijadikita</h1>
     </div>
