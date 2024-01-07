cd03be8ea943905c5341095639998f7bea2b345f
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-01-07 15:54:55 +0700

    HAR-19 Create initiate verifyme page.

---
+++ src/pages/VerifyMePage.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
+import { useEffect, useState } from "react"
+
+const VerifyMePage = () => {
+    const navigate = useNavigate()
+    const { userAcc, setUserAcc } = UserAuth()
+
+    const verifyMe = () => {
+        // setUserAcc(prev => ({...prev, email_verified: true}))
+        console.log(userAcc)
+    }
+    const [isVerified, setIsVerified] = useState(false)
+    useEffect(() => {
+        if(userAcc?.email_verified) {
+            console.log('go to home')
+            // setIsVerified(true)
+            navigate('/home')
+        }
+    }, [userAcc])
+    return (
+        <div style={{ flexDirection: 'column', padding: 50 }}>
+            <h1>Verifikasi Akun Saya</h1>
+            <button style={{ padding: 10, marginTop: 15 }} onClick={() => setUserAcc(prev => ({...prev, email_verified: true}))}>Verifikasi Email Saya</button>
+        </div>
+    )
+}
+
+export default VerifyMePage
