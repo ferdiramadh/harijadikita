467ab19750a06a10e5c6a5913512fe224d67ac96
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-01-14 18:47:59 +0700

    HAR-19 Update join page.

--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -1,16 +1,16 @@
 import React, { ReactElement } from 'react'
-import RinPer1NamaUndanganPage from './RinPer1NamaUndanganPage'
-import RinPer2NamaPengantinPage from './RinPer2NamaPengantinPage'
+import RinPer1NamaUndanganPage from '../components/join/RinPer1NamaUndanganPage'
+import RinPer2NamaPengantinPage from '../components/join/RinPer2NamaPengantinPage'
 import { UseMultiStepForm } from '../hooks/Join/UseMultiStepForm'
 import RinPerButtonSection from '../components/RinPerButtonSection'
-import RinPer3UsernameInstagramPage from './RinPer3UsernameInstagramPage'
-import RinPer4KeluargaPengantinPriaPage from './RinPer4KeluargaPengantinPriaPage'
-import RinPer5KeluargaPengantinWanitaPage from './RinPer5KeluargaPengantinWanitaPage'
-import RinPer6TanggalWaktuPage from './RinPer6TanggalWaktuPage'
-import RinPer7LokasiPage from './RinPer7LokasiPage'
-import RinPer8TambahRekPage from './RinPer8TambahRekPage'
-import RinPer9JumlahTamuPage from './RinPer9JumlahTamuPage'
-import RinPer10TahuDariManaPage from './RinPer10TahuDariManaPage'
+import RinPer3UsernameInstagramPage from '../components/join/RinPer3UsernameInstagramPage'
+import RinPer4KeluargaPengantinPriaPage from '../components/join/RinPer4KeluargaPengantinPriaPage'
+import RinPer5KeluargaPengantinWanitaPage from '../components/join/RinPer5KeluargaPengantinWanitaPage'
+import RinPer6TanggalWaktuPage from '../components/join/RinPer6TanggalWaktuPage'
+import RinPer7LokasiPage from '../components/join/RinPer7LokasiPage'
+import RinPer8TambahRekPage from '../components/join/RinPer8TambahRekPage'
+import RinPer9JumlahTamuPage from '../components/join/RinPer9JumlahTamuPage'
+import RinPer10TahuDariManaPage from '../components/join/RinPer10TahuDariManaPage'
 import RinPerProgress from '../components/RinPerProgress'
 
 export type ButtonType = {
@@ -24,22 +24,23 @@ export type JoinPageType = ButtonType & {
   isLastStep: boolean
   isFirstStep: boolean
 }
+const PageList = [
+  <RinPer1NamaUndanganPage />,
+  <RinPer2NamaPengantinPage />,
+  <RinPer3UsernameInstagramPage />,
+  <RinPer4KeluargaPengantinPriaPage />,
+  <RinPer5KeluargaPengantinWanitaPage />,
+  <RinPer6TanggalWaktuPage />,
+  <RinPer7LokasiPage />,
+  <RinPer8TambahRekPage />,
+  <RinPer9JumlahTamuPage />,
+  <RinPer10TahuDariManaPage />,
+]
 
 const JoinPage = () => {
 
-  const PageList = [
-    <RinPer1NamaUndanganPage />,
-    <RinPer2NamaPengantinPage />,
-    <RinPer3UsernameInstagramPage />,
-    <RinPer4KeluargaPengantinPriaPage />,
-    <RinPer5KeluargaPengantinWanitaPage />,
-    <RinPer6TanggalWaktuPage />,
-    <RinPer7LokasiPage />,
-    <RinPer8TambahRekPage />,
-    <RinPer9JumlahTamuPage />,
-    <RinPer10TahuDariManaPage />,
-  ]
   const { step, steps, currentStepIndex, next, back, isFirstStep, isLastStep } = UseMultiStepForm(PageList)
+
   return (
     <section className='template'>
       <form>
