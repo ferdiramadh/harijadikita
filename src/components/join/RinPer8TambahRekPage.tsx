5822126f069e701fc1bd4b7d913644554002dff8
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-03-05 22:13:13 +0700

    HAR-19 Implement SearchableDropdown in RinPer8TambahRekPage.

--- src/components/join/RinPer8TambahRekPage.tsx
+++ src/components/join/RinPer8TambahRekPage.tsx
@@ -1,6 +1,9 @@
 import { JoinPageType } from "../../pages/JoinPage"
 import TopSection from "../TopSection"
 import { IoIosCloseCircle } from "react-icons/io"
+import SearchableDropdown from "./SearchableDropdown"
+import { useState } from "react"
+import { BankList } from "../../utils/BankList"
 
 type RinPer8TambahRekType = {
     namaRekening: string
@@ -29,18 +32,30 @@ const RinPer8TambahRekPage = ({ namaRekening, namaBank, noRek, namaRekening2, na
         e.preventDefault()
         setAddRekening(!addRekening)
     }
+    const [value, setValue] = useState("Pilih Bank...");
     return (
         <>
-            <TopSection title="Tambah Rekening" tagline="Masukkan rekening kamu untuk menerima hadiah uang cashless dari tamu yang kamu undang."  />
+            <TopSection title="Tambah Rekening" tagline="Masukkan rekening kamu untuk menerima hadiah uang cashless dari tamu yang kamu undang." />
             <div className='form_container'>
                 {addRekening && <div className="title">
                     <h1 className="reception_title">Pemilik rekening ke-1</h1>
                 </div>}
                 <input placeholder="Nama pemilik rekening " type="text" value={namaRekening} onChange={e => updateData({ namaRekening: e.target.value })} />
-                <input placeholder="Pilih bank" type="text" value={namaBank} onChange={e => updateData({ namaBank: e.target.value })} />
+                {/* <div>
+                    <input placeholder="Pilih bank" type="text" value={namaBank} onChange={e => updateData({ namaBank: e.target.value })} />
+                </div> */}
+
+                <SearchableDropdown
+                    options={BankList}
+                    label="name"
+                    id="id"
+                    selectedVal={value}
+                    handleChange={(val: any) => setValue(val)}
+                />
                 <input placeholder="Masukkan no. rekening" type="text" value={noRek} onChange={e => updateData({ noRek: e.target.value })} />
                 {!addRekening && <a href="/" onClick={addingRekening}>Tambah rekening</a>}
                 {addRekening && <AddRekening onClick={addingRekening} namaRekening2={namaRekening2} namaBank2={namaBank2} noRek2={noRek2} updateData={updateData} />}
+
             </div>
 
         </>
@@ -48,7 +63,7 @@ const RinPer8TambahRekPage = ({ namaRekening, namaBank, noRek, namaRekening2, na
 }
 
 const AddRekening = ({ onClick, namaRekening2, namaBank2, noRek2, updateData }: AddRekeningProp) => {
-
+    const [value, setValue] = useState("Pilih Bank...");
     return (
         <>
             <div className="title">
@@ -56,7 +71,14 @@ const AddRekening = ({ onClick, namaRekening2, namaBank2, noRek2, updateData }: 
                 <button className="close_reception_btn" onClick={onClick}> <IoIosCloseCircle color="#474747" size={20} /></button>
             </div>
             <input placeholder="Nama pemilik rekening " type="text" value={namaRekening2} onChange={e => updateData({ namaRekening2: e.target.value })} />
-            <input placeholder="Pilih bank" type="text" value={namaBank2} onChange={e => updateData({ namaBank2: e.target.value })} />
+            {/* <input placeholder="Pilih bank" type="text" value={namaBank2} onChange={e => updateData({ namaBank2: e.target.value })} /> */}
+            <SearchableDropdown
+                    options={BankList}
+                    label="name"
+                    id="id"
+                    selectedVal={value}
+                    handleChange={(val: any) => setValue(val)}
+                />
             <input placeholder="Masukkan no. rekening" type="text" value={noRek2} onChange={e => updateData({ noRek2: e.target.value })} />
         </>
     )
