5bb94a62cba2de826015db41290687fe8558e7ed
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-05-17 22:46:49 +0700

    HAR-25 Implement rincian pernikahan item.

--- src/components/rincian_desain_edit/RincianPernikahan.tsx
+++ src/components/rincian_desain_edit/RincianPernikahan.tsx
@@ -1,49 +1,19 @@
-import { useState } from 'react'
-import { IoIosArrowForward } from "react-icons/io"
+import NamaUndanganItem from "./rincian_pernikahan_item/NamaUndanganItem"
+import NamaPengantinItem from "./rincian_pernikahan_item/NamaPengantinItem"
+import InstagramPengantinItem from "./rincian_pernikahan_item/InstagramPengantinItem"
+import KeluargaPengantinItem from "./rincian_pernikahan_item/KeluargaPengantinItem"
+import JadwalLokasiItem from "./rincian_pernikahan_item/JadwalLokasiItem"
+import RekeningPengantinItem from "./rincian_pernikahan_item/RekeningPengantinItem"
 
 const RincianPernikahan = () => {
   return (
     <div style={{ width: '100%' }}>
-      <RincianPernikahanItem />
-      <RincianPernikahanItem />
-    </div>
-  )
-}
-
-const RincianPernikahanItem = () => {
-  const [toggle, setToggle] = useState(false)
-  const styleArrow = {
-    transform: toggle ? 'rotate(90deg)' : '',
-    transition: 'transform 150ms ease', // smooth transition
-  }
-  const styleContent = toggle ? {
-    maxHeight: 500,
-    transition: 'max-height 0.5s ease-in',
-  } : {
-    maxHeight: 0,
-    overflow: 'hidden',
-    transition: 'max-height 0.15s ease-out',
-  }
-  const onPress = () => {
-    setToggle(!toggle)
-  }
-  return (
-    <div>
-      <div className='rinper-item'>
-        <div className="title">
-          <h3>Nama Undangan</h3>
-        </div>
-        <div className="btn-arrow-wrapper">
-          <button className='btn-arrow' onClick={onPress} style={styleArrow}>
-            <IoIosArrowForward size={20} color='#667085' />
-          </button>
-        </div>
-      </div>
-      <div className="content" style={styleContent}>
-        <div>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora dolorum omnis nisi dolor. Ratione, ipsam dicta libero, optio blanditiis fugit pariatur, perspiciatis amet esse dolore praesentium exercitationem asperiores illo quia.
-        </div>
-      </div>
+      <NamaUndanganItem />
+      <NamaPengantinItem />
+      <InstagramPengantinItem />
+      <KeluargaPengantinItem />
+      <JadwalLokasiItem />
+      <RekeningPengantinItem />
     </div>
   )
 }
