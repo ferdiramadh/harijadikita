e3d3edec67ef2afacaadbfeb841928dd6bae91bd
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-06-10 01:34:11 +0700

    HAR-25 Add getDefaultMiddleware and set serializableCheck: false.

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -10,7 +10,10 @@ export const store = configureStore({
     reducer: {
         counter: counterReducer,
         rinper: rinperReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
 })
 
 export type RootState = ReturnType<typeof store.getState>
