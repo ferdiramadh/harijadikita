12075ca19926da9dcd5f032423f36202f3c8103d
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-06-15 19:42:44 +0700

    HAR-25 Create bottom fix menu.

---
+++ src/components/rincian_desain_edit/BottomMenu.tsx
@@ -0,0 +1,18 @@
+
+
+function BottomMenu() {
+    return (
+        <div className='bottomFixContainer'>
+            <div className="bottomWrapper">
+                <button className="draftBtn" onClick={() => null}>
+                    Simpan draft
+                </button>
+                <button className="shareBtn" onClick={() => null}>
+                    Publikasikan
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default BottomMenu
