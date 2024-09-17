959a61211a79c4a9dc580b77ae07e4a970e9b0aa
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-09-17 13:47:49 +0700

    HAR-27 Develop UploadGambarSection.

--- src/components/rincian_desain_edit/desain_undangan_item/UploadGambarSection.tsx
+++ src/components/rincian_desain_edit/desain_undangan_item/UploadGambarSection.tsx
@@ -1,22 +1,65 @@
 import { FiUploadCloud } from "react-icons/fi"
 import { useDropzone } from 'react-dropzone'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import { storage } from "../../../firebase"
+import { ref, uploadString } from "firebase/storage"
 
 type UploadImageType = {
     titleLable: string
+    onImageChange: (value: string | ArrayBuffer | null | undefined) => void
 }
 
-const UploadGambarSection = ({titleLable} : UploadImageType) => {
+const UploadGambarSection = ({ titleLable, onImageChange }: UploadImageType) => {
 
-    const onDrop = useCallback((acceptedFiles: any) => {
-        // Do something with the files
-        const file = new FileReader
-        file.onload = function () {
-            console.log(file.result)
+    const [image, setImage] = useState<string | ArrayBuffer | null | undefined>()
+    const [count, setCount] = useState(0)
+    const storageRef = ref(storage, `Sampul/Images/${count}`)
+    const uploadImage = async () => {
+        try {
+            uploadString(storageRef, `${image}`, 'data_url').then((snapshot) => {
+
+                console.log(snapshot);
+              });
+              
+        } catch (error) {
+            
         }
-        file.readAsDataURL(acceptedFiles[0])
-    }, [])
-    const { acceptedFiles, getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+    }
+    const onDrop = useCallback((acceptedFiles:File[]) => {
+        acceptedFiles.map((file) => {
+          const reader = new FileReader();
+    
+          reader.onload = function (e) {
+            let img = e.target?.result
+            setImage(img)
+          };
+    
+          reader.readAsDataURL(file);
+          return file;
+        });
+      }, []);
+    const { acceptedFiles, getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept: {
+            'image/png': ['.png'],
+            'image/jpeg': ['.jpg', 'jpeg'],
+        },
+        maxSize: 2 * 1024 * 1024,
+        multiple: false
+    })
+    useEffect(() => {
+        if(image) {
+            onImageChange(image)
+        }
+    }, [image])
+    if(image) return  <div style={{paddingBottom: 150}}>
+        <h3>{count}</h3>
+    <img src={`${image}`} style={{width: '100%', }}
+    ></img>
+    <button onClick={uploadImage}>upload</button>
+    <button onClick={() => setImage('')}>remove</button>
+    <button onClick={() => setCount(prev => prev + 1)}>add count</button>
+    </div>
     return (
         <>
             <label className="label_input">{titleLable}</label>
