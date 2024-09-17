3e479f519129bcdec97eda7d411c23aced535ba4
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-09-17 13:46:49 +0700

    HAR-27 Develop SampulItem.

--- src/components/rincian_desain_edit/desain_undangan_item/SampulItem.tsx
+++ src/components/rincian_desain_edit/desain_undangan_item/SampulItem.tsx
@@ -1,21 +1,49 @@
 import DesainUndanganItem from "./DesainUndanganItem"
 import UploadGambarSection from "./UploadGambarSection"
+import { SampulType } from "../../../redux/state/desainundangan/desainUndanganSlice"
 
-const SampulItem = () => {
+type SampulItemType = {
+    sampulItemData: Partial<SampulType>
+    setSampulItemData: React.Dispatch<React.SetStateAction<Partial<SampulType>>>
+}
+const SampulItem = ({ sampulItemData, setSampulItemData }: SampulItemType) => {
+    const onToggle = () => {
+        setSampulItemData(prev => {
+            return {
+                ...prev,
+                isActive: !prev.isActive
+            }
+        })
+    }
     return (
         <DesainUndanganItem
             title="Sampul"
-            children={<Content />}
+            children={<Content sampulItemData={sampulItemData} setSampulItemData={setSampulItemData} />}
+            toggleVal={sampulItemData.isActive}
+            onToggle={onToggle}
         />
     )
 }
 
-const Content = () => {
+const Content = ({ sampulItemData, setSampulItemData }: SampulItemType) => {
+    const onImageChange = (value: string | ArrayBuffer | null | undefined) => {
+        setSampulItemData(prev => {
+            return {
+                ...prev,
+                gambarBackground: value
+            }
+        })
+    }
     return (
         <div className="content_wrapper">
             <label className="label_input">Teks pada tombol</label>
-            <input type="text" placeholder="Buka undangan" />
-            <UploadGambarSection titleLable="Gambar background"/>
+            <input type="text" placeholder="Buka undangan" value={sampulItemData.teksTombol} onChange={e => setSampulItemData(prev => {
+                return {
+                    ...prev,
+                    teksTombol: e.target.value
+                }
+            })} />
+            <UploadGambarSection titleLable="Gambar background" onImageChange={onImageChange} />
         </div>
     )
 }
