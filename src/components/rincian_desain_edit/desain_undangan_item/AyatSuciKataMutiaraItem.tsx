1a05186016a9013ccbe3a3e37c8d4faaa765b80c
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-09-17 19:48:20 +0700

    HAR-27 Implement and update AyatSuciKataMutiaraItem.

--- src/components/rincian_desain_edit/desain_undangan_item/AyatSuciKataMutiaraItem.tsx
+++ src/components/rincian_desain_edit/desain_undangan_item/AyatSuciKataMutiaraItem.tsx
@@ -1,62 +1,120 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import DesainUndanganItem from './DesainUndanganItem'
 import SearchableDropdown from '../../join/SearchableDropdown'
+import { AyatSuciKalimatMutiaraType } from '../../../redux/state/desainundangan/desainUndanganSlice'
 
-function AyatSuciKataMutiaraItem() {
+type AyatSuciKataMutiaraItemType = {
+    ayaSuciKataMutiaraItemData: Partial<AyatSuciKalimatMutiaraType>
+    setAyaSuciKataMutiaraItemData: React.Dispatch<React.SetStateAction<Partial<AyatSuciKalimatMutiaraType>>>
+}
+
+const AyatSuciKataMutiaraItem = ({ ayaSuciKataMutiaraItemData, setAyaSuciKataMutiaraItemData }: AyatSuciKataMutiaraItemType) => {
+    const onToggle = () => {
+        setAyaSuciKataMutiaraItemData(prev => {
+            return {
+                ...prev,
+                isActive: !prev.isActive
+            }
+        })
+    }
     return (
         <DesainUndanganItem
             title="Ayat suci/kalimat mutiara"
-            children={<Content />}
+            children={<Content ayaSuciKataMutiaraItemData={ayaSuciKataMutiaraItemData} setAyaSuciKataMutiaraItemData={setAyaSuciKataMutiaraItemData} />}
+            toggleVal={ayaSuciKataMutiaraItemData.isActive}
+            onToggle={onToggle}
         />
     )
 }
 
-const Content = () => {
-    const [isAyat, setIsAyat] = useState(true)
+const Content = ({ ayaSuciKataMutiaraItemData, setAyaSuciKataMutiaraItemData }: AyatSuciKataMutiaraItemType) => {
+
     const [religion, setReligion] = useState([
         {
             id: 1,
-            name: "Buddha"
+            name: "Buddha",
+            verse: `“Kebahagiaan duniawi terbesar yang dapat dialami manusia adalah perpaduan dari pernikahan yang mengikat dua hati yang saling mencintai menjadi satu.”
+(Sutta Pitaka – Digha Nikaya)`
         },
         {
             id: 2,
-            name: "Hindu"
+            name: "Hindu",
+            verse: `Dalam sebuah pernikahan kalian disatukan demi sebuah kebahagiaan dengan janji hati untuk saling membahagiakan. Bersamaku engkau akan hidup selamanya karena Tuhan pasti akan memberikan karunia sebagai pelindung dan saksi dalam pernikahan ini. Untuk itulah kalian dipersatukan dalam satu keluarga. (Rgveda : X.85.36)`
         },
         {
             id: 3,
-            name: "Islam"
+            name: "Islam",
+            verse: `"Di antara tanda-tanda (kebesaran)-Nya ialah bahwa Dia menciptakan pasangan-pasangan untukmu dari (jenis) dirimu sendiri agar kamu merasa tenteram kepadanya. Dia menjadikan di antaramu rasa cinta dan kasih sayang.” (Surah Ar Rum Ayat 21)`
         },
         {
             id: 4,
-            name: "Katolik/Protestan"
+            name: "Katolik/Protestan",
+            verse: `“Demikianlah mereka bukan lagi dua, melainkan satu. Karena itu, apa yang telah dipersatukan Allah, tidak boleh diceraikan manusia.” (Matius 19:6)`
         },
         {
             id: 5,
-            name: "Khonghucu"
+            name: "Khonghucu",
+            verse: `“Keselarasan hidup bersama anak istri itu laksana alat musik yang ditabuh harmonis. Kerukunan diantara kakak dan adik itu membangun damai dan bahagia. Maka demikianlah hendaknya engkau berbuat di dalam rumah tanggamu, bahagiakanlah istri dan anak-anakmu.” (Kitab Shi Jing BAB XIV. Setapak Demi Setapak)`
         },
     ]
     )
     const [value, setValue] = useState(religion[2].name)
+
+    const onToggleIsAyatSuci = () => {
+        setAyaSuciKataMutiaraItemData(prev => {
+            return {
+                ...prev,
+                isAyatSuci: !prev.isAyatSuci
+            }
+        })
+    }
+    useEffect(() => {
+        if (value)
+            setAyaSuciKataMutiaraItemData(prev => {
+                let choseReligion = religion.filter((item, i) => item.name == value)
+                return {
+                    ...prev,
+                    agama: choseReligion[0].name,
+                    ayatSuci: choseReligion[0].verse
+                }
+            })
+    }, [value])
+
     return (
         <div className="content_wrapper">
             <div className="radioBtnWrapper">
-                <input className='radioBtn' type="radio" checked={isAyat} onClick={() => setIsAyat(!isAyat)}/>
+                <input className='radioBtn' type="radio" checked={ayaSuciKataMutiaraItemData.isAyatSuci} onClick={onToggleIsAyatSuci} />
                 <p>Pilih ayat suci</p>
             </div>
-            <SearchableDropdown
-                options={religion}
-                label="name"
-                id="id"
-                selectedVal={value}
-                handleChange={(val: any) => setValue(val)}
-                isRinPerPage={true}
-                objectName='Agama'
-            />
+            {
+                ayaSuciKataMutiaraItemData.isAyatSuci
+                &&
+                <SearchableDropdown
+                    options={religion}
+                    label="name"
+                    id="id"
+                    selectedVal={value}
+                    handleChange={(val: any) => setValue(val)}
+                    isRinPerPage={true}
+                    objectName='Agama'
+                />
+            }
+
             <div className="radioBtnWrapper">
-                <input className='radioBtn' type="radio" checked={!isAyat} onClick={() => setIsAyat(!isAyat)}/>
+                <input className='radioBtn' type="radio" checked={!ayaSuciKataMutiaraItemData.isAyatSuci} onClick={onToggleIsAyatSuci} />
                 <p>Buat kalimat mutiara</p>
             </div>
-            <textarea id="w3review" name="w3review" rows={6} cols={50} className='large_input_area'></textarea>
+            {
+                !ayaSuciKataMutiaraItemData.isAyatSuci
+                &&
+                <textarea id="w3review" name="w3review" rows={6} cols={50} className='large_input_area' value={ayaSuciKataMutiaraItemData.kalimatMutiara} onChange={e => setAyaSuciKataMutiaraItemData(prev => {
+                    return {
+                        ...prev,
+                        kalimatMutiara: e.target.value
+                    }
+                })} />
+            }
+
         </div>
     )
 }
