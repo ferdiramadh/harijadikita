e0f6e4b6a8a66c3972ea1b50c30995b949e1de8f
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-09-25 00:04:17 +0700

    HAR-27 Implement LoadingOverlay on login page.

--- src/components/LoginRegis.tsx
+++ src/components/LoginRegis.tsx
@@ -1,12 +1,13 @@
 import SvgGoogle from '../assets/Icon/Google.svg'
 import SvgFb from '../assets/Icon/Facebook.svg'
 import TopSection from './TopSection'
-import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
 import { useEffect, useState } from 'react'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../firebase'
 import { FirebaseError } from '@firebase/util'
+import LoadingOverlay from 'react-loading-overlay-ts'
 
 type LoginRegisProps = {
     title: string
@@ -18,37 +19,44 @@ type LoginRegisProps = {
 type VerificationProp = Partial<LoginRegisProps> & {
     title: string
     isEmailVerification?: boolean
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const LoginRegis = ({ title, tagLine, isLogin, isEmailVerification = false }: LoginRegisProps) => {
     const masuk = isLogin ? "Daftar" : "Masuk"
     const noAccount = isLogin ? "Tidak" : "Sudah"
-
+    const [loading, setLoading] = useState(false)
     return (
-        <section className='template'>
-            <TopSection title={title} tagline={tagLine} isLoginPage={true} />
-            <InputEmailPassSection title={title} isEmailVerification={isEmailVerification} />
-            {
-                !isEmailVerification && <hr />
+        <LoadingOverlay
+            active={loading}
+            spinner
+            text='Mohon tunggu...'
+        >
+            <section className='template'>
+                <TopSection title={title} tagline={tagLine} isLoginPage={true} />
+                <InputEmailPassSection title={title} isEmailVerification={isEmailVerification} setLoading={setLoading} />
+                {
+                    !isEmailVerification && <hr />
 
-            }
+                }
 
-            {
-                !isEmailVerification && <GoogleFbSection title={title} isLogin={isLogin} />
+                {
+                    !isEmailVerification && <GoogleFbSection title={title} isLogin={isLogin} />
 
-            }
+                }
 
-            {!isEmailVerification && isLogin && <p><NavLink to='/resetpassword' style={({ isActive, isPending, isTransitioning }) => {
-                return {
-                    color: isPending ? "red" : "#474747",
-                };
-            }}>Lupa Password?</NavLink></p>}
-            {!isEmailVerification && <p>{noAccount} punya akun?  <NavLink to={isLogin ? '/regis' : '/signin'}>{masuk}</NavLink></p>}
-        </section>
+                {!isEmailVerification && isLogin && <p><NavLink to='/resetpassword' style={({ isActive, isPending, isTransitioning }) => {
+                    return {
+                        color: isPending ? "red" : "#474747",
+                    };
+                }}>Lupa Password?</NavLink></p>}
+                {!isEmailVerification && <p>{noAccount} punya akun?  <NavLink to={isLogin ? '/regis' : '/signin'}>{masuk}</NavLink></p>}
+            </section>
+        </LoadingOverlay>
     )
 }
 
-const InputEmailPassSection = ({ title, isEmailVerification }: VerificationProp) => {
+const InputEmailPassSection = ({ title, isEmailVerification, setLoading }: VerificationProp) => {
 
     const [error, setError] = useState('')
     const isRegis = () => {
@@ -58,7 +66,7 @@ const InputEmailPassSection = ({ title, isEmailVerification }: VerificationProp)
     const { setUserAcc, user } = UserAuth()
     const handleVerification = async (e: any) => {
         e.preventDefault()
-        console.log('email ver')
+        // console.log('email ver')
         // const resultEmailVer = await sendEmailVerification(user)
         // if(resultEmailVer == null) {
         //     setUserAcc(prev => ({...prev, email_verified: true}))
@@ -66,25 +74,32 @@ const InputEmailPassSection = ({ title, isEmailVerification }: VerificationProp)
     }
     const handleOnSubmit = async (e: any) => {
         e.preventDefault()
+        setLoading(true)
         const email = e.target.email.value
         const password = e.target.password.value
         const name = isRegis() ? e.target.nama.value : ''
         try {
             setUserAcc(prev => ({ ...prev, displayName: name }))
             const result = isRegis() ? await createUserWithEmailAndPassword(auth, email, password) : await signInWithEmailAndPassword(auth, email, password)
-            const {user} = result
+            const { user } = result
             if (user) {
                 localStorage.setItem('items', JSON.stringify(user));
             }
+            setLoading(false)
         } catch (error: unknown) {
             if (error instanceof FirebaseError) {
                 setError(error.code)
             }
+            setLoading(false)
         }
     }
     return (
         <>
-            <form action="" className='btn_form_container' onSubmit={isEmailVerification ? (e) => handleVerification(e) : (e) => handleOnSubmit(e)}>
+            <form
+                action=""
+                className='btn_form_container'
+                onSubmit={isEmailVerification ? (e) => handleVerification(e) : (e) => handleOnSubmit(e)}
+            >
                 {error && <p style={{ color: 'red' }}>{error}</p>}
                 {!isEmailVerification && <input placeholder="Email" type="text" name='email' />}
                 {!isEmailVerification && isRegis() && <input placeholder="Nama" type="text" name="nama" />}
@@ -97,7 +112,7 @@ const InputEmailPassSection = ({ title, isEmailVerification }: VerificationProp)
     )
 }
 
-const GoogleFbSection = ({ title, isLogin }: VerificationProp) => {
+const GoogleFbSection = ({ title, isLogin }: Partial<VerificationProp>) => {
 
     const navigate = useNavigate()
     const { googleSignIn, userAcc, facebookSignIn, setUserAcc, user } = UserAuth()
