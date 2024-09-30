336ad6ff99cb8e5b5583d3b478120df8af475e3a
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2024-09-30 23:52:52 +0700

    HAR-42 Implement no image option on pengantin item.

--- src/components/rincian_desain_edit/desain_undangan_item/PengantinItem.tsx
+++ src/components/rincian_desain_edit/desain_undangan_item/PengantinItem.tsx
@@ -43,10 +43,31 @@ const Content = ({ pengantinItemData, setPengantinItemData }: PengantinItemType)
             }
         })
     }
+    const onToggle = () => {
+        setPengantinItemData(prev => {
+            return {
+                ...prev,
+                isNoImage: !prev.isNoImage
+            }
+        })
+    }
     return (
         <div className="content_wrapper">
-            <UploadGambarSection titleLable="Gambar pengantin pria" onImageChange={onImageChangePengantinPria} sectionFolder='Pengatin_Pria' photoUrl={pengantinItemData.gambarPengantinPria}/>
-            <UploadGambarSection titleLable="Gambar pengantin wanita" onImageChange={onImageChangePengantinWanita} sectionFolder='Pengatin_Wanita' photoUrl={pengantinItemData.gambarPengantinWanita}/>
+            <div className="radioBtnWrapper">
+                <input className='radioBtn' type="radio" checked={pengantinItemData.isNoImage} onClick={onToggle} />
+                <p>Tanpa gambar atau foto</p>
+            </div>
+            <div className="radioBtnWrapper">
+                <input className='radioBtn' type="radio" checked={!pengantinItemData.isNoImage} onClick={onToggle} />
+                <p>Pakai gambar atau foto</p>
+            </div>
+            {
+                !pengantinItemData.isNoImage &&
+                <>
+                    <UploadGambarSection titleLable="Gambar pengantin pria" onImageChange={onImageChangePengantinPria} sectionFolder='Pengatin_Pria' photoUrl={pengantinItemData.gambarPengantinPria} />
+                    <UploadGambarSection titleLable="Gambar pengantin wanita" onImageChange={onImageChangePengantinWanita} sectionFolder='Pengatin_Wanita' photoUrl={pengantinItemData.gambarPengantinWanita} />
+                </>
+            }
         </div>
     )
 }
