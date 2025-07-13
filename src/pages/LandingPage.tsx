d29c3bbc6e23c18b449962bcf052c8ca75a2e673
DESKTOP-A577LD0\herla <herlambangdes@gmail.com>
2025-07-13 20:13:20 +0700

    Ubah landing page.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -9,11 +9,69 @@ const LandingPage = () => {
     return '/home'
   }
   return (
-    <div style={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center', padding: 20 }}>
-      <div>
-        <h1>Landing Page Ceritanya</h1>
-        <Link to={checkSignIn()}>Sign In</Link>
-      </div>
+    <div className="grid-container">
+
+      <header>
+        <img
+          src="https://ik.imagekit.io/herlambangdes/Asset/Harijadikita/harijadikita%20logo%20combination.svg?updatedAt=1751369135547"
+          className="logo"
+        />
+      </header>
+
+      <section className="left">
+        <div className="hero-container">
+          <div className="copywriting">
+            <h1 className="headline text-focus-in">
+              Buat undangan nikah digital cuma
+              <ul className="underline">
+                <li className="li-1">5 menit.</li>
+                <li className="li-2">Rp199K.</li>
+              </ul>
+            </h1>
+            <p className="description text-focus-in">
+              Undang banyak tamu ke hari jadi pernikahan kamu lebih mudah,
+              cepat, dan modern dengan berbagai tema undangan nikah digital yang
+              cantik.
+            </p>
+          </div>
+          <div className="cta">
+            {/* <button href="#" className="btn-buat-undangan fade-in-heartbeat">
+              Buat undangan sekarang
+            </button>
+            <button href="#" className="btn-tema-undangan">
+              Lihat tema undangan
+            </button> */}
+          </div>
+        </div>
+
+
+      </section>
+      <section className="right">
+        <div className="parent">
+          <div className="photo-container">
+            <div className="item-1">
+              <img
+                src="https://ik.imagekit.io/herlambangdes/Asset/Harijadikita/Couple-1.jpg?updatedAt=1750670881163"
+              />
+            </div>
+            <div className="item-2">
+              <img
+                src="https://ik.imagekit.io/herlambangdes/Asset/Harijadikita/Couple-2.jpg?updatedAt=1750671082686"
+              />
+            </div>
+            <div className="item-3">
+              <img
+                src="https://ik.imagekit.io/herlambangdes/Asset/Harijadikita/Couple-3.jpg?updatedAt=1750671209086"
+              />
+            </div>
+            <div className="item-4">
+              <img
+                src="https://ik.imagekit.io/herlambangdes/Asset/Harijadikita/Couple-4.jpg?updatedAt=1750671287768"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
 
   )
