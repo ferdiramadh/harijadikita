3b9111e8932c0fcbf8a1d1590e0415c4843d1802
ferdiramadh <ferdiramadh@gmail.com>
2025-09-07 18:26:44 +0700

    HAR-20 Implement log out on account page.

--- src/pages/TemporerHomePage.tsx
+++ src/pages/TemporerHomePage.tsx
@@ -1,32 +1,17 @@
 import { UserAuth } from '../context/AuthContext'
-import { useDispatch, useSelector } from 'react-redux'
-import { AppDispatch, RootState } from '../redux/store'
-import { useNavigate } from 'react-router-dom'
-import { resetDesainUndangan } from '../redux/state/desainundangan/desainUndanganSlice'
-import HamburgerMenu from '../components/HamburgerMenu'
+import { useSelector } from 'react-redux'
+import { RootState } from '../redux/store'
 
 const TemporerHomePage = () => {
 
-  const navigate = useNavigate()
   const { uid, email } = useSelector((state: RootState) => state.user)
-  const { logOut, userAcc, user, setUserAcc } = UserAuth()
-  const dispatch = useDispatch<AppDispatch>()
-  const handleLogout = async () => {
-    try {
-      dispatch(resetDesainUndangan())
-      await logOut()
-      setUserAcc({})
-    } catch (error) {
-      alert(error)
-    }
-  }
+  const { userAcc } = UserAuth()
 
   return (
     <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start', padding: 20, flexDirection: 'column', marginTop: 60 }}>
-     {/* <HamburgerMenu /> */}
+      {/* <HamburgerMenu /> */}
       <div style={{ display: 'flex', height: '100%', justifyContent: 'flex-start', alignItems: 'flex-start', padding: 20, flexDirection: 'column' }}>
         <h3>Welcome, {userAcc?.displayName} {uid} {email}</h3>
-        {user ? <button onClick={handleLogout}>Logout</button> : null}
         {/* <button onClick={() => navigate("/rinciandesain")}>GO To Edit Rincian Desain </button> */}
       </div>
     </div>
